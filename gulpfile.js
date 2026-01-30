f4e3ee15ff1d0be31f2fcf705f419e70538d7ae8
Bruno Salcedo Cadiz <132630771+BrunoSC32@users.noreply.github.com>
2026-01-30 00:08:11 -0400

    Add Gulp tasks for JavaScript and update HTML for font loading

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-import { src, dest, watch } from "gulp";
+import { src, dest, watch, series } from "gulp";
 import * as sass from "sass";
 import gulpSass from "gulp-sass";
 
@@ -11,7 +11,14 @@ export function styles(){
     .pipe(dest("build/css"))
 }
 
+export function scripts() {
+  return src("src/js/app.js")
+    .pipe(dest("build/js"))
+}
 
 export function dev() {
   watch("src/scss/**/*.scss", styles)
+  watch("src/js/**/*.js", scripts)
 }
+
+export default series( styles, scripts, dev)
