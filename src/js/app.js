a54ca9b5525cab4dfa34b8ac754fc13e65e4a6fa
Bruno Salcedo Cadiz <132630771+BrunoSC32@users.noreply.github.com>
2026-02-01 16:10:19 -0400

    feat: add img modal to gallery

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     loadGallery()
 })
 
+
 function loadGallery() {
 
     const NUM_IMG = 16
@@ -14,9 +15,52 @@ function loadGallery() {
         img.src = `src/img/gallery/full/${i}.jpg`
         img.alt = 'image gallery'
 
+        img.onclick = () => {
+            modalImage(i)
+        }
+
         gallery.appendChild(img)
 
     }
+}
 
+function modalImage(i) {
+
+    // Create a element IMG 
+    const img = document.createElement('IMG')
+    img.src = `src/img/gallery/full/${i}.jpg`
+    img.alt = 'image gallery'
+
+    // Create a elemento div for the modal
+    const modal = document.createElement('DIV')
+    modal.classList.add('modal')
+    modal.onclick = closeModal
+
+    // Create close button
+    const closeButton = document.createElement('BUTTON')
+    closeButton.textContent = 'X'
+    closeButton.classList.add('btn-close')
+    closeButton.onClick = closeModal
+
+    modal.appendChild(img)
+    modal.appendChild(closeButton)
+
+    const body = document.querySelector('body')
+    body.classList.add('overflow-hidden')
+    body.appendChild(modal)
+
+}
+
+function closeModal() {
+
+    const modal = document.querySelector('.modal')
+    modal.classList.add('fadeOut')
+
+    setTimeout(() => {
+        modal?.remove();
+
+        const body = document.querySelector('body')
+        body.classList.remove('overflow-hidden')
+    }, 500)
 
 }
